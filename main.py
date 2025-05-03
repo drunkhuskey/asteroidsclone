b672be8d57054f276438ee4a93f8d53c93937688
drunkhuskey <sam.orent@gmail.com>
2025-05-03 06:31:13 -0400

    added black display

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+
+# source venv/bin/activate
 import pygame
+from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Game loop display
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        pygame.display.flip()
+    
+    
     
 if __name__ == "__main__":
     main()
