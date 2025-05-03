7878e28bec238ef017d0431b59439416fd3777dc
drunkhuskey <sam.orent@gmail.com>
2025-05-03 06:36:40 -0400

    added 60 fps

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0 # delta time since last tick
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    # Game loop display
+    # =============================== Game loop start ========================================
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,6 +26,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000 # 60 fps
     
     
     
